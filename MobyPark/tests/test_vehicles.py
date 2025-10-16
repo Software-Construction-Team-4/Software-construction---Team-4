2953d1de121124b649290be861367336ca23de55
QXOonincx <lakeyfn.70@gmail.com>
2025-10-16 11:15:37 +0200

    comments verbeterd

--- MobyPark/tests/test_vehicles.py
+++ MobyPark/tests/test_vehicles.py
@@ -17,18 +17,15 @@ def test_vehicle_endpoints(vehicle):
         "name": "MyCar"
     }
 
-    # vehicle aanmaken
     resp = requests.post(f"{BASE_URL}/vehicles", headers=headers, json=data)
     assert resp.status_code in [200, 201]
     vid = resp.text.split(":")[-1].strip()
 
-    # vehicle ophalen
     resp = requests.get(f"{BASE_URL}/vehicles/{vid}", headers=headers)
     assert resp.status_code == 200
     v_data = resp.json()
     assert v_data["license_plate"] == "AB-123-CD"
 
-    # vehicle updaten
     update_data = {
         "license_plate": "XY-987-ZT",
         "make": "Honda",
@@ -40,13 +37,11 @@ def test_vehicle_endpoints(vehicle):
     resp = requests.put(f"{BASE_URL}/vehicles/{vid}", headers=headers, json=update_data)
     assert resp.status_code == 200
 
-    # data ophalen om te kijken of update is gelukt
     resp = requests.get(f"{BASE_URL}/vehicles/{vid}", headers=headers)
     assert resp.status_code == 200
     v_data = resp.json()
     assert v_data["license_plate"] == "XY-987-ZT"
     assert v_data["year"] == 2025
 
-    # vehicle verwijderen
     resp = requests.delete(f"{BASE_URL}/vehicles/{vid}", headers=headers)
     assert resp.status_code == 200
