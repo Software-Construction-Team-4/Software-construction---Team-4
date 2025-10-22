9f5eac8a5ceaa1bb3173048366e7e43fecef0b57
Sina0p <sinahashemi134@gmail.com>
2025-10-22 19:14:17 +0200

    intergration test for login endpoint made

---
+++ MobyPark/tests/test_login.py
@@ -0,0 +1,36 @@
+import requests
+import pytest
+
+BASE_URL = "http://localhost:8000"
+
+def test_register_endpoint():
+    DummyUserOne ={
+        "username": "sina",
+        "password": "321"
+    }
+
+    DummyUserTwo ={
+        "username": "sina",
+        "password": "3211"
+    }
+
+    DummyUserThree ={
+        "username": "NietBestaand",
+        "password": "321"
+    }
+
+    FirstResult = requests.post(f"{BASE_URL}/login", json = DummyUserOne)
+
+    data = FirstResult.json()
+    assert FirstResult.status_code == 200
+    assert data["message"] == "User logged in"
+
+    SecondResult = requests.post(f"{BASE_URL}/login", json = DummyUserTwo)
+
+    assert SecondResult.status_code == 401
+    assert SecondResult.text == "Invalid credentials"
+
+    ThirdResult = requests.post(f"{BASE_URL}/login", json = DummyUserThree)
+
+    assert ThirdResult.status_code == 401
+    assert ThirdResult.text == "User not found"
