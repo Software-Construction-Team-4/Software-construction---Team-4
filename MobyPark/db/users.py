6e48adbcf9a38bc6391f55ae91093b9e05b3a530
ioasaph <61933670+ioasaph@users.noreply.github.com>
2025-11-04 13:45:56 +0100

    add static user getters

--- MobyPark/db/users.py
+++ MobyPark/db/users.py
@@ -3,7 +3,8 @@ import mysql.connector
 from enum import StrEnum
 from datetime import datetime
 
-db = mysql.connector.connect(host="localhost", user="admin", password="admin")
+db = mysql.connector.connect(host="localhost", user="root", password="admin", database="mobypark")
+cursor = db.cursor()
 
 class UserRole(StrEnum):
     ADMIN = "ADMIN"
@@ -13,7 +14,7 @@ class User:
     CREATION_DATE_FORMAT: str = "%Y-%m-%d"
 
     def __init__(self, id: int, username: str, password: str, name: str, email: Optional[str] = None, phone: Optional[str] = None, role: Optional[UserRole] = UserRole.USER,
-                 created_at: str = datetime.now().strftime(CREATION_DATE_FORMAT), birth_year: Optional[int] = None, active: bool = True) -> None:
+                 created_at: str = datetime.now().strftime(CREATION_DATE_FORMAT), birth_year: Optional[int] = None, active: bool = True) -> 'User':
         self.id = id
         self.username = username
         self.password = password
@@ -24,3 +25,13 @@ class User:
         self.created_at = datetime.strptime(created_at, User.CREATION_DATE_FORMAT)
         self.birth_year = birth_year
         self.active = active
+
+    @staticmethod
+    def get_by_id(id: int) -> Optional['User']:
+        result = cursor.fetchone("SELECT * FROM users WHERE id = ?", (id,))
+        return (result is not None) and User(*result) or None
+
+    @staticmethod
+    def get_by_username(username: str) -> Optional['User']:
+        result = cursor.fetchone("SELECT * FROM users WHERE LOWER(username) = ?", (username.lower(),))
+        return (result is not None) and User(*result) or None
