139ce56d1989baf96943f1b334c0fb4a31061f56
Sina0p <sinahashemi134@gmail.com>
2025-11-18 19:20:55 +0100

    usermodel af

--- MobyPark/session_manager.py
+++ MobyPark/session_manager.py
@@ -1,15 +1,10 @@
 sessions = {}
 
 def add_session(token, user):
-    """
-    Store a session for a logged-in user.
-    The `user` dict must include at least: username and user_id.
-    """
-    # Ensure we store only safe fields
     sessions[token] = {
-        "username": user.get("username"),
-        "user_id": user.get("id") or user.get("user_id"),
-        "role": user.get("role")
+        "username": user.username,
+        "user_id": user.id,
+        "role": user.role
     }
 
 def remove_session(token):
