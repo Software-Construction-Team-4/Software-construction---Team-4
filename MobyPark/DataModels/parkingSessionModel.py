eae8f426f3370c96621c392a08d51c28322ff019
LucaN <118000599+Zycloak@users.noreply.github.com>
2025-11-18 15:37:58 +0100

    parking session model

---
+++ MobyPark/DataModels/parkingSessionModel.py
@@ -0,0 +1,14 @@
+import datetime
+from decimal import Decimal
+
+class ParkingSession:
+    def __init__(self, id: int, parking_lot_id: int, user_id: int, licenseplate: str, started: datetime.datetime, stopped: datetime.datetime, duration_minutes: int, cost: Decimal, payment_status: str):
+        self.id = id
+        self.parking_lot_id = parking_lot_id
+        self.user_id = user_id
+        self.licenseplate = licenseplate
+        self.started = started
+        self.stopped = stopped
+        self.duration_minutes = duration_minutes
+        self.cost = cost
+        self.payment_status = payment_status
