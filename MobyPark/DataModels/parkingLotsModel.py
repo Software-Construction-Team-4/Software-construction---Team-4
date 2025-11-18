7d1b5a55db6cae7fc1f33d3e86599b37a665a8b3
LucaN <118000599+Zycloak@users.noreply.github.com>
2025-11-18 13:10:39 +0100

    datamodels

---
+++ MobyPark/DataModels/parkingLotsModel.py
@@ -0,0 +1,20 @@
+import datetime
+from decimal import Decimal
+
+class ParkingLot:
+    def __init__(self, id: int, name: str, location: str, address: str, capacity: int, reserved: int, tariff: Decimal, daytariff: Decimal, 
+                 created_at: datetime.date, latitude: Decimal, longitude: Decimal, status: str, closed_reason: str, closed_date: datetime.date):
+        self.id = id
+        self.name = name
+        self.location = location
+        self.address = address
+        self.capacity = capacity
+        self.reserved = reserved
+        self.tariff = tariff
+        self.daytariff = daytariff
+        self.created_at = created_at
+        self.latitude = latitude
+        self.longitude = longitude
+        self.status = status
+        self.closed_reason = closed_reason
+        self.closed_date = closed_date
