c6f203f25f00cecb05c5726274f96f6a5e8f4804
ssoares <S.Soaress@outlook.com>
2025-11-18 14:08:31 +0100

    Payments access done

--- MobyPark/DataAccesLayer/PaymentsAccess.py
+++ MobyPark/DataAccesLayer/PaymentsAccess.py
@@ -20,239 +20,167 @@ class PaymentsDataAccess:
             cursor = conn.cursor(dictionary=True)
             cursor.execute("SELECT * FROM payments")
             rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
+            return [PaymentsModel(**row) for row in rows]
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
     
-    def get_by_id(self, id : int):
+    def get_by_id(self, id: int):
         try:
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE id = {id}")
+            cursor.execute("SELECT * FROM payments WHERE id = %s", (id,))
             rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
+            return [PaymentsModel(**row) for row in rows]
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
     
-    def get_by_payment_method(self, method : str):
+    def get_by_payment_method(self, method: str):
         try:
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE payment_method = {method}")
+            cursor.execute("SELECT * FROM payments WHERE payment_method = %s", (method,))
             rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
+            return [PaymentsModel(**row) for row in rows]
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_bank(self, bank : str):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE bank = {bank}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_transaction_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE transaction_date = {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_created_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE created_at = {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_before_transaction_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE transaction_date =< {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_after_transaction_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE transaction_date => {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_before_created_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE created_at =< {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_after_created_date(self, date : datetime):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE created_at => {date}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_parking_lot_id(self, id : int):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE parking_lot_id = {id}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_session_id(self, id : int):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE session_id = {id}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
-    
-    def get_by_initiator(self, name : str):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE initiator = {name}")
-            rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
-        finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
 
-    def get_by_transaction_date(self, code : str):
+    def get_by_bank(self, bank: str):
         try:
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE issuer_code = {code}")
+            cursor.execute("SELECT * FROM payments WHERE bank = %s", (bank,))
             rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
+            return [PaymentsModel(**row) for row in rows]
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
+    
+    def get_by_transaction_date(self, date: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE transaction_date = %s", (date,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
 
-    def get_between_dates(self, date_min : datetime, date_max : datetime):
+    def get_by_created_date(self, date: datetime.datetime):
         try:
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
-            cursor.execute(f"SELECT * FROM payments WHERE created_at => {date_min} AND created_at =< {date_max}")
+            cursor.execute("SELECT * FROM payments WHERE created_at = %s", (date,))
             rows = cursor.fetchall()
-            
-            payments = [PaymentsModel(**row) for row in rows]
-            return payments
-        except Error as e:
-            print("Error reading data from MySQL:", e)
+            return [PaymentsModel(**row) for row in rows]
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
+
+    def get_before_transaction_date(self, date: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE transaction_date <= %s", (date,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_after_transaction_date(self, date: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE transaction_date >= %s", (date,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_before_created_date(self, date: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE created_at <= %s", (date,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_after_created_date(self, date: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE created_at >= %s", (date,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_by_parking_lot_id(self, id: int):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE parking_lot_id = %s", (id,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_by_session_id(self, id: int):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE session_id = %s", (id,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_by_initiator(self, name: str):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE initiator = %s", (name,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_by_issuer_code(self, code: str):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM payments WHERE issuer_code = %s", (code,))
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_between_dates(self, date_min: datetime.datetime, date_max: datetime.datetime):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(
+                "SELECT * FROM payments WHERE created_at >= %s AND created_at <= %s",
+                (date_min, date_max),
+            )
+            rows = cursor.fetchall()
+            return [PaymentsModel(**row) for row in rows]
+        finally:
+            cursor.close()
+            conn.close()
 
     def update_payment(self, updated_p: PaymentsModel):
         try:
@@ -277,11 +205,60 @@ class PaymentsDataAccess:
                 WHERE id = %s
             """
 
-            cursor.execute(sql, (updated_p
-            ))
-
+            cursor.execute(sql, updated_p.to_update_tuple())
             conn.commit()
             return True
+
+        except Error as e:
+            print("Error updating data:", e)
+            return False
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    def delete_payment(self, id : int):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM payments WHERE id = %s", (id,))
+            conn.commit()
+
+        except Error as e:
+            print("Error updating data:", e)
+            return False
+        
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+    
+    def insert_payment(self, payment : PaymentsModel):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            sql = """
+            INSERT INTO payments (
+                amount,
+                completed_at,
+                created_at,
+                payment_hash,
+                initiator,
+                parking_lot_id,
+                session_id,
+                bank,
+                transaction_date,
+                issuer_code,
+                payment_method,
+                transaction_hash
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            cursor.execute(sql, payment.to_tuple())
+            conn.commit()
+            return cursor.lastrowid
 
         except Error as e:
             print("Error updating data:", e)
