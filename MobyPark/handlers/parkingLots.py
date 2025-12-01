da471a7d95318434dcf9d2bd559b6d010b766236
QXOonincx <lakeyfn.70@gmail.com>
2025-12-01 14:26:12 +0100

    parking lots met datamodel en kleine error mesage toegvoegd

--- MobyPark/handlers/parkingLots.py
+++ MobyPark/handlers/parkingLots.py
@@ -24,13 +24,16 @@ def do_GET(self):
 
     if parts[0] == "parking-lots":
         if len(parts) == 1:
-            lots = load_parking_lots()
-            return send_json(self, 200, lots)
+            lots = load_parking_lots()  # dict[id] -> ParkingLot
+            lots_serialized = {
+                lot_id: lot.__dict__ for lot_id, lot in lots.items()
+            }
+            return send_json(self, 200, lots_serialized)
 
         if len(parts) == 2:
-            lot = load_parking_lot_by_id(parts[1])
+            lot = load_parking_lot_by_id(parts[1])  # ParkingLot or None
             if lot:
-                return send_json(self, 200, lot)
+                return send_json(self, 200, lot.__dict__)
             else:
                 return send_json(self, 404, {"error": "Not found"})
 
@@ -65,6 +68,11 @@ def do_PUT(self):
 def do_DELETE(self):
     parts = self.path.strip("/").split("/")
     if parts[0] == "parking-lots" and len(parts) == 2:
+
+        lot = load_parking_lot_by_id(parts[1])
+        if not lot:
+            return send_json(self, 404, {"error": "Parking lot does not exist"})
+
         delete_parking_lot(parts[1])
         return send_json(self, 200, {"message": "Deleted"})
 
