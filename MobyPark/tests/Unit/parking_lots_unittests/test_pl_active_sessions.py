663ba9c0e2dcaea9f32b6cee91f5403e49255392
LucaN <Nootje2003@gmail.com>
2025-12-18 00:20:24 +0100

    meer unit tests voor edge cases

--- MobyPark/tests/Unit/parking_lots_unittests/test_pl_active_sessions.py
+++ MobyPark/tests/Unit/parking_lots_unittests/test_pl_active_sessions.py
@@ -2,8 +2,7 @@ import unittest
 from unittest.mock import MagicMock, patch
 from DataAccesLayer import db_utils_parkingLots as parkingLot_utils
 
-
-class ParkingLotsActiveSessions(unittest.TestCase):
+class UnitTestParkingLotsActiveSessions(unittest.TestCase):
 
     def test_active_sessions(self):
         with patch("DataAccesLayer.db_utils_parkingLots.get_db_connection") as db_connection:
@@ -11,9 +10,12 @@ class ParkingLotsActiveSessions(unittest.TestCase):
             db_connection.return_value.cursor.return_value = db_cursor
 
             parkingLot_utils.increment_active_sessions(67, 1)
-
             self.assertTrue(db_cursor.execute.called)
 
+            db_cursor.reset_mock()
+
+            parkingLot_utils.increment_active_sessions(67, -2)
+            self.assertTrue(db_cursor.execute.called)
 
 if __name__ == "__main__":
     unittest.main()
