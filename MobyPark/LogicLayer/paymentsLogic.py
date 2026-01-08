7e9647fbf6f0dce9882bef8a4217481aca4a1a9c
Sina0p <sinahashemi134@gmail.com>
2026-01-08 22:37:57 +0100

    parking session proces een goed stuk verbeterd

--- MobyPark/LogicLayer/paymentsLogic.py
+++ MobyPark/LogicLayer/paymentsLogic.py
@@ -1,25 +1,33 @@
-from DataModels.paymentsModel import PaymentsModel
-from datetime import datetime
-import session_calculator as sc
-from DataAccesLayer.PaymentsAccess import PaymentsDataAccess
+import random
 
-class paymentsLogic:
-    def create_payment(session, amount, bank, payment_method, initiator):
-        payment = PaymentsModel(
-            amount=amount,
-            created_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            completed_at=None,
-            payment_hash=sc.generate_payment_hash(),
-            initiator=initiator,
-            parking_lot_id=session.parking_lot_id,
-            session_id=session.id,
-            bank=bank,
-            transaction_date=None,
-            issuer_code=None,
-            payment_method=payment_method,
-            transaction_hash=sc.generate_transaction_validation_hash(
-                session.id, session.licenseplate
-            )
-        )
+def create_issuer_code():
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    code = ""
 
-        return PaymentsDataAccess().insert_payment(payment)
+    list = [1,2,3,4,5,6,7,8]
+
+    for i in list:
+        code += random.choice(characters)
+
+    return code
+
+# class paymentsLogic:
+#     def create_payment(session, amount, bank, payment_method, initiator):
+#         payment = PaymentsModel(
+#             amount=amount,
+#             created_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#             completed_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#             payment_hash=sc.generate_payment_hash(),
+#             initiator=initiator,
+#             parking_lot_id=session.parking_lot_id,
+#             session_id=session.id,
+#             bank=bank,
+#             transaction_date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#             issuer_code=create_issuer_code(),
+#             payment_method=payment_method,
+#             transaction_hash=sc.generate_transaction_validation_hash(
+#                 session.id, session.licenseplate
+#             )
+#         )
+
+#         return PaymentsDataAccess().insert_payment(payment)
