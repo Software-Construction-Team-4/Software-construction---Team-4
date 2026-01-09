3a30e33e3f03db65729d25e9ced9beb6c9a52310
Sina0p <sinahashemi134@gmail.com>
2026-01-09 17:56:51 +0100

    reservation expired

--- MobyPark/task.py
+++ MobyPark/task.py
@@ -1,7 +1,9 @@
-# MobyPark/task.py
 from datetime import datetime
+from DataAccesLayer.db_utils_reservations import cehck_and_update_reservation_status_12_hours
 
 def run():
+    cehck_and_update_reservation_status_12_hours()
+    
     with open("/var/log/cron.log", "a") as f:
         f.write(f"[{datetime.now()}] 12-hour task ran\n")
 
