d680c405873d5ca220b08322c03cb221605dd73b
Sina0p <sinahashemi134@gmail.com>
2026-01-10 17:36:00 +0100

    reservation cost is nu standaard op null

--- MobyPark/handlers/reservations.py
+++ MobyPark/handlers/reservations.py
@@ -58,7 +58,7 @@ def do_POST(self):
             end_time=data["end_time"],
             status="pending",
             created_at=datetime.now(),
-            cost=data["cost"],
+            cost=None,
             updated_at=None
         )
 
