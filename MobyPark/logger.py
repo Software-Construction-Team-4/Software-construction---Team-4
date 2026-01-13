3fcf2d0dfd75e2d9098fa829823f0f36051d0dfd
ioasaph <61933670+ioasaph@users.noreply.github.com>
2026-01-13 14:56:20 +0100

    Add remote logging class

---
+++ MobyPark/logger.py
@@ -0,0 +1,54 @@
+from enum import StrEnum
+import os
+from typing import Union
+import requests # should probably use aiohttp
+import datetime
+
+
+class Logger:
+    class Level(StrEnum):
+        INFO = "info"
+        WARN = "warn"
+        ERROR = "error"
+
+
+    DISCORD_WEBHOOK_URL =  "https://discord.com/api/webhooks/1459289003890180117/LEOzMqwLe4kqnfYwh7f6TqJ6BBFEktBWTwpwCBZ41pGlZGeH9PxrWnk6fHA36frRBMca"
+    # os.getenv("DISCORD_WEBHOOK_URL")
+
+    FORMAT_TIMESTAMP = "<t:{timestamp}:R>"
+    FORMAT_ERROR = "```py\n{exception}\n```"
+
+
+    @staticmethod
+    def log(message: str, level: Level = Level.INFO, mention: bool = False, colour: int = 0x252525) -> None:
+        timestamp: int = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+        body = {
+            "content": "@everyone" if mention else None,
+            "embeds": [
+                {
+                    "title": f"[{level.upper()}]",
+                    "color": colour,
+                    "fields": [
+                        {
+                            "name": "When?",
+                            "value": Logger.FORMAT_TIMESTAMP.format(timestamp = timestamp)
+                        },
+                        {
+                            "name": "Details",
+                            "value": message
+                        }
+                    ]
+                }
+            ]
+        }
+
+        requests.post(Logger.DISCORD_WEBHOOK_URL, json=body)
+
+    @staticmethod
+    def warn(message: str) -> None:
+        Logger.log(message, level = Logger.Level.WARN, colour = 0xF9BB37)
+
+    @staticmethod
+    def error(exception: Union[Exception, str]) -> None:
+        Logger.log(f"An error has occurred!\n{Logger.FORMAT_ERROR.format(exception = str(exception))}",
+                   level = Logger.Level.ERROR, mention = True, colour = 0xDB3C1D)
