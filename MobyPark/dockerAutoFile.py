617b927508cf90164fd94a1a4b5bcd9c6942e592
Sina0p <sinahashemi134@gmail.com>
2026-01-19 15:09:01 +0100

    testing for automatisation

--- MobyPark/dockerAutoFile.py
+++ MobyPark/dockerAutoFile.py
@@ -1,41 +1,41 @@
 import mysql.connector
 from datetime import datetime
-from DataAccesLayer.db_utils_reservations import create_missed_parking_sessions
+# from DataAccesLayer.db_utils_reservations import create_missed_parking_sessions
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host="145.24.237.71",
-        port=8001,
-        user="vscode",
-        password="StrongPassword123!",
-        database="mobypark"
-    )
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="145.24.237.71",
+#         port=8001,
+#         user="vscode",
+#         password="StrongPassword123!",
+#         database="mobypark"
+#     )
 
-def pending_to_expired():
-    conn = get_db_connection()
-    cursor = conn.cursor()
+# def pending_to_expired():
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
 
-    try:
-        cursor.execute("""
-            UPDATE reservations
-            SET status = 'expired'
-            WHERE status = 'pending'
-            AND end_time < NOW()
-        """)
-        conn.commit()
-        return cursor.rowcount
+#     try:
+#         cursor.execute("""
+#             UPDATE reservations
+#             SET status = 'expired'
+#             WHERE status = 'pending'
+#             AND end_time < NOW()
+#         """)
+#         conn.commit()
+#         return cursor.rowcount
 
-    finally:
-        cursor.close()
-        conn.close()
+#     finally:
+#         cursor.close()
+#         conn.close()
 
 def run():
-    create_missed_parking_sessions()
-    pending_to_expired()
+    # create_missed_parking_sessions()
+    # pending_to_expired()
 
     with open("/var/log/cron.log", "a") as f:
         f.write(f"[{datetime.now()}] 12-hour task ran\n")
-        Logger.log("Ran 12-hour task.")
+        # Logger.log("Ran 12-hour task.")
 
 if __name__ == "__main__":
     run()
