0de435a56bcd4b67f6d71e56612ae7a4212af559
LucaN <Nootje2003@gmail.com>
2026-01-19 16:44:51 +0100

    docker 24 uur log function fix

--- MobyPark/dockerAutoFile.py
+++ MobyPark/dockerAutoFile.py
@@ -1,37 +1,37 @@
 import mysql.connector
-from datetime import datetime
-# from DataAccesLayer.db_utils_reservations import create_missed_parking_sessions
+from datetime import datetime, date
+from LogicLayer.reservationsLogic import process_missed_sessions
 
-# def get_db_connection():
-#     return mysql.connector.connect(
-#         host="145.24.237.71",
-#         port=8001,
-#         user="vscode",
-#         password="StrongPassword123!",
-#         database="mobypark"
-#     )
+def get_db_connection():
+    return mysql.connector.connect(
+        host="145.24.237.71",
+        port=8001,
+        user="vscode",
+        password="StrongPassword123!",
+        database="mobypark"
+    )
 
-# def pending_to_expired():
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
+def pending_to_expired():
+    conn = get_db_connection()
+    cursor = conn.cursor()
 
-#     try:
-#         cursor.execute("""
-#             UPDATE reservations
-#             SET status = 'expired'
-#             WHERE status = 'pending'
-#             AND end_time < NOW()
-#         """)
-#         conn.commit()
-#         return cursor.rowcount
+    try:
+        cursor.execute("""
+            UPDATE reservations
+            SET status = 'expired'
+            WHERE status = 'pending'
+            AND end_time < NOW()
+        """)
+        conn.commit()
+        return cursor.rowcount
 
-#     finally:
-#         cursor.close()
-#         conn.close()
+    finally:
+        cursor.close()
+        conn.close()
 
 def run():
-    # create_missed_parking_sessions()
-    # pending_to_expired()
+    process_missed_sessions(date.today())
+    pending_to_expired()
 
     with open("/var/log/cron.log", "a") as f:
         f.write(f"[{datetime.now()}] 12-hour task ran\n")
