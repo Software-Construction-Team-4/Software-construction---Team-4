5040d61bb5da520a1bf4e3b474df27e5cbba1bbd
clearwwaters <61933670+ioasaph@users.noreply.github.com>
2026-01-21 20:30:14 +0100

    Fix docker auto file to log to Discord

--- MobyPark/dockerAutoFile.py
+++ MobyPark/dockerAutoFile.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from logger import Logger
 from datetime import datetime, date
 from DataAccesLayer.db_utils_reservations import pending_to_expired
 from DataAccesLayer.db_utils_parkingSessions import create_parking_sessions_from_expired_reservations
@@ -10,7 +11,7 @@ def run():
 
     with open("/var/log/cron.log", "a") as f:
         f.write(f"[{datetime.now()}] 12-hour task ran\n")
-        # Logger.log("Ran 12-hour task.")
+        Logger.log("Ran 12-hour task.")
 
 if __name__ == "__main__":
     run()
